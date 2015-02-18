f5d6b4dc2e89d1996400c2e1894fcad1a6b61638
chouclee <chouclee@cmu.edu>
2015-02-17 19:54:32 -0500

    refactor

--- btree/SRC/stack.c
+++ btree/SRC/stack.c
@@ -1,42 +1,48 @@
 #include "stack.h"
 #include "def.h"
 
+/* Initialize the stack with default allocation size*/
 void stack_init(stack *s,int elemSize)
 {
 	assert(elemSize > 0);
 	s->elemSize = elemSize;
 	s->N = 0;
-	s->capacity = stackInitialAllocationSize;
-	s->data = malloc(stackInitialAllocationSize * elemSize);
+	s->capacity = StackInitialAllocationSize;
+	s->data = malloc(StackInitialAllocationSize * elemSize);
 	assert(s->data != NULL);
 }
 
-void stack_destory(stack *s)
-{
-	free(s->data);
-}
-
+/* Check whether stack is empty or not*/
 int stack_isEmpty(const stack *s) {
 	if (s->N == 0)
 		return TRUE;
 	else return FALSE;
 }
+
+/* Push an element into stack*/
 void stack_push(stack *s, const void *src) {
-	void *dest;
 	if(s->N == s->capacity){
 		s->capacity *= 2;
 		s->data = realloc(s->data, s->capacity * s->elemSize);
 		assert(s->data != NULL);
 	}
-	dest = (char *)s->data + s->N * s->elemSize;
+	void *dest = (char *)s->data + s->N * s->elemSize;
 	memcpy(dest, src, s->elemSize);
 	s->N++;
 }
+
+/* Pop an element*/
 void stack_pop(stack *s, void *dst) {
 	const void *src;
 	assert(stack_isEmpty(s) == FALSE);
 	s->N--;
 	src = (const char *)s->data + s->N * s->elemSize;
 	memcpy(dst, src, s->elemSize);
+}
+
+/* Free memory of the stack*/
+void stack_destory(stack *s)
+{
+	free(s->data);
 }
 
